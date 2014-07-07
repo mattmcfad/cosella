e5f4808a7fddf9cc075e07b5515e4b98744f48de
Matt McFadyen <mmcfadyen91@gmail.com>
2014-07-07 04:08:11 -0400

    update :octocat:s and added quotes

--- dev/javascript/octocats.js
+++ dev/javascript/octocats.js
@@ -1,57 +1,93 @@
 var octocats = [
 	{
-		img: 'bear-cavalry.png'
+		img: 'daftpunktocat-guy.gif',
+		lvl: 13,
+		msg: 'We are human after all.',
+		button: 'Much in common after all.'
 	},
 	{
-		img: 'daftpunktocat-guy.gif'
+		img: 'heisencat.png',
+		lvl: 11,
+		msg: "We're done when I say we're done.",
+		button: "You're god damn right."
 	},
 	{
-		img: 'heisencat.png'
+		img: 'homercat.png',
+		lvl: 5,
+		msg: 'Operator! Give me the number for 911!',
+		button: 'Doh!'
 	},
 	{
-		img: 'homercat.png'
+		img: 'ironcat.jpg',
+		lvl: 1,
+		msg: 'Tony Stark was able to build this in a cave! Can you beat the next level?',
+		button: 'I am Iron Man.'
 	},
 	{
-		img: 'ironcat.jpg'
+		img: 'murakamicat.png',
+		lvl: 9,
+		msg: 'Pain is inevitable. Suffering is optional.',
+		button: '?!?!????'
 	},
 	{
-		img: 'murakamicat.png'
+		img: 'nyantocat.gif',
+		lvl: 15,
+		msg: 'Okay dude, you really play this game too much.',
+		button: 'Nyanyanyanyanyanyanya!'
 	},
 	{
-		img: 'nyantocat.gif'
+		img: 'octobiwan.jpg',
+		lvl: 4,
+		msg: 'Use the Force, Luke.',
+		button: "I'm not Luke."
 	},
 	{
-		img: 'octobiwan.jpg'
+		img: 'spidertocat.png',
+		lvl: 3,
+		msg: 'Who am I?',
+		button: "I'm Spider-Man!"
 	},
 	{
-		img: 'spidertocat.png'
+		img: 'visionary.jpg',
+		lvl: 12,
+		msg: 'Stay hungry. Stay foolish.',
+		button: 'Thanks, Steve.'
 	},
 	{
-		img: 'steroidtocat.png'
+		img: 'xtocat.jpg',
+		lvl: 8,
+		msg: "Don't $^#& this up.",
+		button: 'Thank you Logan. I mean that.'
 	},
 	{
-		img: 'topguntocat.png'
+		img: 'riddlocat.png',
+		lvl: 10,
+		msg: 'Riddle me this, Riddle me that.',
+		button: 'Who is afraid of the big bad bat?'
 	},
 	{
-		img: 'visionary.jpg'
+		img: 'stormtroopocat.png',
+		lvl: 2,
+		msg: "These aren't the droids we're looking for.",
+		button: 'Move along.. move along'
 	},
 	{
-		img: 'xtocat.jpg'
+		img: 'okal-eltocat.jpg',
+		lvl: 6,
+		msg: 'This looks like a job for',
+		button: 'Superman!'
 	},
 	{
-		img: 'riddlocat.png'
+		img: 'dodgetocat_v2.png',
+		lvl: 7,
+		msg: 'If you can dodge a wrench, you can dodge a ball.',
+		button: 'Put me in, coach!'
 	},
 	{
-		img: 'stormtroopocat.png'
-	},
-	{
-		img: 'okal-eltocat.jpg'
-	},
-	{
-		img: 'dodgetocat_v2.png'
-	},
-	{
-		img: 'daftpunktocat-thomas.gif'
+		img: 'daftpunktocat-thomas.gif',
+		lvl: 14,
+		msg: 'Work it harder, make it better, do it faster',
+		button: 'make us stronger.'
 	}
 
 ];
